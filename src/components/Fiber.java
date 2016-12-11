78dcb139fac2e360274d1004b9bb95bfbd1cdf6d
Borna Ivanković <borna.ivankovic@fer.hr>
2016-12-11 16:29:48 +0100

    Minor improvements
    Added fiber length text
    Component popup updates can be saved

--- src/components/Fiber.java
+++ src/components/Fiber.java
@@ -8,28 +8,32 @@ public class Fiber {
 	private double attenuance;
 	public OpticalComponent c1;
 	public OpticalComponent c2;
-	
-	public Fiber(OpticalComponent c1,OpticalComponent c2){
-		this.c1=c1;
-		this.c2=c2;
+
+	public Fiber(OpticalComponent c1, OpticalComponent c2) {
+		this.c1 = c1;
+		this.c2 = c2;
 	}
-	
+
 	public double getLength() {
 		return length;
 	}
+
 	public void setLength(double length) {
 		this.length = length;
 	}
+
 	public double getAttenuance() {
 		return attenuance;
 	}
+
 	public void setAttenuance(double attenuance) {
 		this.attenuance = attenuance;
 	}
-	
-	public void draw(Graphics g){
+
+	public void draw(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.drawLine(c1.getP().x, c1.getP().y, c2.getP().x, c2.getP().y);
+		g.drawString(String.valueOf(length), (c1.getP().x + c2.getP().x) / 2, (c1.getP().y + c2.getP().y) / 2);
 	}
 
 }
--- src/gui/Popup.java
+++ src/gui/Popup.java
@@ -3,30 +3,34 @@ package gui;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import components.OpticalComponent;
 
-public class Popup extends JFrame{
+public class Popup extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-	
-	public Popup(MouseEvent e, ArrayList<OpticalComponent> komponente){
+	private ArrayList<OpticalComponent> komponente;
+
+	public Popup(MouseEvent e, ArrayList<OpticalComponent> komponente) {
 		this.setLocation(e.getPoint());
 		this.setSize(300, 300);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{31, 64, 86, 0};
-		gridBagLayout.rowHeights = new int[]{31, 20, 20, 20, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 31, 64, 86, 0 };
+		gridBagLayout.rowHeights = new int[] { 31, 20, 20, 20, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		getContentPane().setLayout(gridBagLayout);
-		
+		this.komponente = komponente;
+
 		JLabel lblNewLabel = new JLabel("Name");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
@@ -34,7 +38,7 @@ public class Popup extends JFrame{
 		gbc_lblNewLabel.gridx = 1;
 		gbc_lblNewLabel.gridy = 1;
 		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.anchor = GridBagConstraints.NORTHWEST;
@@ -43,7 +47,7 @@ public class Popup extends JFrame{
 		gbc_textField.gridy = 1;
 		getContentPane().add(textField, gbc_textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Insertion loss");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
@@ -51,7 +55,7 @@ public class Popup extends JFrame{
 		gbc_lblNewLabel_1.gridx = 1;
 		gbc_lblNewLabel_1.gridy = 2;
 		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.anchor = GridBagConstraints.NORTHWEST;
@@ -60,29 +64,47 @@ public class Popup extends JFrame{
 		gbc_textField_1.gridy = 2;
 		getContentPane().add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblReturnLoss = new JLabel("Return loss");
 		GridBagConstraints gbc_lblReturnLoss = new GridBagConstraints();
 		gbc_lblReturnLoss.anchor = GridBagConstraints.EAST;
-		gbc_lblReturnLoss.insets = new Insets(0, 0, 0, 5);
+		gbc_lblReturnLoss.insets = new Insets(0, 0, 5, 5);
 		gbc_lblReturnLoss.gridx = 1;
 		gbc_lblReturnLoss.gridy = 3;
 		getContentPane().add(lblReturnLoss, gbc_lblReturnLoss);
-		
+
 		textField_2 = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_2.anchor = GridBagConstraints.NORTHWEST;
 		gbc_textField_2.gridx = 2;
 		gbc_textField_2.gridy = 3;
 		getContentPane().add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
+
+		JButton btnSave = new JButton("Save");
+		btnSave.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				for (OpticalComponent c : komponente) {
+					if (c.isSelected()) {
+						c.setLabel(textField.getText());
+					}
+				}
+			}
+		});
+		GridBagConstraints gbc_btnSave = new GridBagConstraints();
+		gbc_btnSave.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSave.gridx = 1;
+		gbc_btnSave.gridy = 7;
+		getContentPane().add(btnSave, gbc_btnSave);
 		for (OpticalComponent c : komponente) {
-			if(c.isSelected()){
+			if (c.isSelected()) {
 				textField.setText(c.getLabel());
 				textField_1.setText(Double.toString(c.getInsertionLoss()));
 				textField_2.setText(Double.toString(c.getReturnLoss()));
 			}
 		}
-		this.setVisible(true);
+		setVisible(true);
 	}
 }
